332c82fc35def7e88bed74122a8bb3bd68b6046a
943996316@qq.com <943996316@qq.com>
2021-08-30 19:20:29 +0800

    完善了侧页面跳转，BUG修复，页面排版修正

--- views.py
+++ views.py
@@ -19,64 +19,64 @@ conn.request("POST", "/rest/campusclientwebsite/v1/journey/nodelist?=&=", payloa
 res = conn.getresponse()
 data = res.read().decode("utf-8")
 data = json.loads(data)
-
-a = 0
-for i in data['data']:
-    try:
-        # 时间戳处理
-        if float(i['accTime']) > 162847546100:
-            i['accTime'] = float(i['accTime']) / 1000
-        timestamp = float(i['accTime'])
-
-        timeArray = time.localtime(timestamp)
-
-        otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
-
-        i['accTime'] = otherStyleTime
-
-    except Exception as e:
-        print("shibai")
-# L1 = []
+print(data['data'])
+# a = 0
+# for i in data['data']:
+#     try:
+#         # 时间戳处理
+#         if float(i['accTime']) > 162847546100:
+#             i['accTime'] = float(i['accTime']) / 1000
+#         timestamp = float(i['accTime'])
+#
+#         timeArray = time.localtime(timestamp)
+#
+#         otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+#
+#         i['accTime'] = otherStyleTime
+#
+#     except Exception as e:
+#         print("shibai")
+# # L1 = []
+# # for i in range(len(data['data'])):
+# #     try:
+# #         L1.append(data['data'][i]['nodeKpi']['duration'])
+# #         print(data['data'][i]['accTime'])
+# #         print(L1)
+# #     except:
+# #         L1.append('异常')
+# # for i in range(30):
+# #     try:
+# #         if int(data["data"][i]['nodeKpi']['duration']) < 1:
+# #             data["data"][i]['nodeKpi']['duration'] = 0
+# #         print(data["data"][i]['nodeKpi']['duration'])
+# #     except:
+# #         print("用户信息错误")
+# import random
+#
+# accTime = []
+# liveTime = []
+# jourDir = {}
+# jourList = []
+# situation = []
+# color = []
 # for i in range(len(data['data'])):
+#     pingFen = random.randrange(80, 100)
+#     accTime.append(data['data'][i]['accTime'])
+#
 #     try:
-#         L1.append(data['data'][i]['nodeKpi']['duration'])
-#         print(data['data'][i]['accTime'])
-#         print(L1)
+#         liveTime.append(data['data'][i]['nodeKpi']['duration'])
+#         # print(data['data'][i]['accTime'])
+#         # print(liveTime)
 #     except:
-#         L1.append('异常')
-# for i in range(30):
-#     try:
-#         if int(data["data"][i]['nodeKpi']['duration']) < 1:
-#             data["data"][i]['nodeKpi']['duration'] = 0
-#         print(data["data"][i]['nodeKpi']['duration'])
-#     except:
-#         print("用户信息错误")
-import random
-
-accTime = []
-liveTime = []
-jourDir = {}
-jourList = []
-situation = []
-color = []
-for i in range(len(data['data'])):
-    pingFen = random.randrange(80, 100)
-    accTime.append(data['data'][i]['accTime'])
-
-    try:
-        liveTime.append(data['data'][i]['nodeKpi']['duration'])
-        # print(data['data'][i]['accTime'])
-        # print(liveTime)
-    except:
-        liveTime.append(0)
-    if liveTime[i] == 0 :
-        situation.append("接入错误")
-        color.append('#FF000')
-    else:
-        situation.append("接入成功")
-        color.append('#5dd375')
-    jourDir = {'accTime': data['data'][i]['accTime'], 'huoyuetime': liveTime[i],
-               'pingfen': pingFen,'color':color[i],}
-
-    jourList.append(jourDir)
-print(situation)
+#         liveTime.append(0)
+#     if liveTime[i] == 0 :
+#         situation.append("接入错误")
+#         color.append('#FF000')
+#     else:
+#         situation.append("接入成功")
+#         color.append('#5dd375')
+#     jourDir = {'accTime': data['data'][i]['accTime'], 'huoyuetime': liveTime[i],
+#                'pingfen': pingFen,'color':color[i],}
+#
+#     jourList.append(jourDir)
+# print(situation)
